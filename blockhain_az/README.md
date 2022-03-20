5d02edccf22da1766ec1d3293d8aa003f089d2f9
Mukarom Alisyaban <mukarom.alisyaban@ovo.id>
2022-03-21 00:53:12 +0700

    blockchain|udemy|practice_1|building and maining blockchain

--- blockhain_az/README.md
+++ blockhain_az/README.md
@@ -1,1 +1,11 @@
+# Reference 
 Ref : https://ovo.udemy.com/course/build-your-blockchain-az/learn/lecture/9657394#overview
+
+# Tools
+- anaconda
+- spyder
+- Flask 0.12.2
+- postman
+
+# Consensus
+Consensus approach Proof of Work (PoW)
--- blockhain_az/blockchain/blockchain.py
+++ blockhain_az/blockchain/blockchain.py
@@ -17,3 +17,40 @@ from flask import Flask, jsonify
 
 
 # Building Blockchain
+class Blockchain:
+    def __init__(self):
+        # init chain list
+        self.chain = []
+        
+        # init genesis block
+        self.create_block(proof = 1, previous_hash = '0')
+    
+    def create_block(self, proof, previous_hash):
+        # init dictionary key that will hold blockchain metadata
+        block = {'index': len(self.chain),
+                 'timestamp': datetime.datetime.now(),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        
+        # append the block onto the chain
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        # get the last block of the chain
+        return self.chain[-1]
+        
+    def proof_of_work(self, proof, previous_proof):
+        # init new proof to iterate over (we will start the proof by 1)
+        new_proof = 1
+        # in order for the iteration to works, we will need to define proof checker
+        check_proof = False
+        while check_proof is False:
+            # define the challange that will be solved by the miner (we will use 4 leading zeros to define the winner, with combination of new proof and previous proof on the formula)
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+        
